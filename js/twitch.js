8729a2feb48046d9ad6b9fe275ea142ce90b44fc
Nicholas Galarza <nicogala93@gmail.com>
2017-06-27 19:30:50 -0400

    Obtained API key, making GET requests

--- js/twitch.js
+++ js/twitch.js
@@ -18,3 +18,61 @@ function getTwitchStreamers(clickEvent, streamList) {
 }
 
 document.getElementById("defaultOpen").click();
+
+$(document).ready(function() {
+
+    var logo, name, bio;
+    // Get info about FCC.
+    var url = "https://api.twitch.tv/kraken/users/freecodecamp";
+    $.ajax({
+        type: 'GET',
+        url: url,
+        headers: {
+            'Client-ID': 'sh9x4gbft40ht9pg5mk0p6sfvn1h3a'
+        },
+        async: true,
+        success: function(data) {
+            console.log(data);
+            name = data.name;
+            bio = data.bio;
+            logo = data.logo;
+        }
+    });
+
+    // Get freeCodeCamp's followers
+    $('#All').html("<p>Targeting all streamers</p>");
+    var url = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels";
+    $.ajax({
+        type: 'GET',
+        url: url,
+        headers: {
+            'Client-ID': 'sh9x4gbft40ht9pg5mk0p6sfvn1h3a'
+        },
+        async: true,
+        success: function(data) {
+            console.log(data);
+            if (data.stream === null) {
+                $('#All').html("FCC is offline!");
+            } else {
+                $('#All').html("FCC is online!");
+            }
+        }
+    });
+
+    // Get random people who are online.
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/kraken/streams',
+        headers: {
+            'Client-ID': 'sh9x4gbft40ht9pg5mk0p6sfvn1h3a'
+        },
+        async: true,
+        success: function(online) {
+            console.log(online);
+        }
+    });
+
+    // Get random people who are offline.
+
+
+});
