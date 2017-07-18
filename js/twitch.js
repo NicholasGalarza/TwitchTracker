bd3697353f8073c5bf3cc5caf615873285538131
NicholasGalarza <nicogala93@gmail.com>
2017-07-17 21:14:28 -0400

    Converting spaghetti code to revealing module pattern

--- js/twitch.js
+++ js/twitch.js
@@ -33,135 +33,124 @@ $(document).ready(function() {
   // Open panel of default button automatically
   document.getElementById("fccButton").click();
 
-  // TODO: If a user is online, display what they are streaming.
-  // TODO: Edit html & css files so that rendered files are better layed out upon load.
-  // TODO: Display data for FreeCodeCamp's followers.
-  // TODO: Figure out how to gather a list of currently active users (preferably random).
-  // TODO: Create a search bar in case a user wants to find a certain user and display if the account is closed.
+/* ========================================= */
 
-  var CLIENT_ID = 'sh9x4gbft40ht9pg5mk0p6sfvn1h3a';
-  var logo, name, status;
-  // TODO: Modular rendering function to reduce lines of code.
-  /* Takes in an array or single value representing info about a streamer.
-   * Parse the information and construct the html with updated field values.
-   * Utilziing JQuery, we can parse that information into the DOM and create a list.
+  // TODO: Make get requst to FreeCodeCamp to gather their followers.
+  // TODO: Protocol for making GET requests for every streamer:
+    // 1.) Check if their accounts exists with 'https://api.twitch.tv/kraken/channels/{streamer-name}'
+      // If user doesn't exist, just place center text saying: {user} DNE or closed their account.
+      // If the account exists & is online, the endpoint will provide all needed information.
+      // Otherwise, use this endpoint: 'https://api.twitch.tv/kraken/users/{streamer-name}' to gather alternate information for offline user.
+    // 2.) All of this means that there will be three types of rendering methods based on if the user is online or not.
+      // renderNonExistantStreamer()
+      // renderOfflineStreamer()
+      // renderOnlineStreamer()
+
+  /* Have module accept name and generate corresponding render field through ajax requests.
    */
-  function renderUser(target, data) {}
-
-  // This is a callback function to determine if user is online after recieving
-  // the streamer's logo and title.
-  function isFccOnline(data) {
-    name = data.name;
-    logo = data.logo;
-    $('#logo').html('<img src=\"' + data.logo + "\">");
-    $('#title').html('<h3>' + data.name + "</h3>");
-
-    var url = 'https://api.twitch.tv/kraken/streams/' + data.name;
-    $.ajax({
-      type: 'GET',
-      url: url,
-      headers: {
-        'Client-ID': CLIENT_ID
-      },
-      async: true,
-      success: function(status) {
-        if (status.stream !== null) { // if online
-          $('#status').html('<i class="fa fa-globe fa-2x" aria-hidden="true"></i>');
-        } else { // if offline
-          $('#status').html('<i class="fa fa-bolt fa-3x" aria-hidden="true"></i>');
-        }
-      }
-    });
-  };
-
-  // This method is specifically for FreeCodeCamp.
-  function getNameAndLogo() {
-    var url = "https://api.twitch.tv/kraken/users/freecodecamp";
-    $.ajax({
-      type: 'GET',
-      url: url,
-      headers: {
-        'Client-ID': CLIENT_ID
-      },
-      async: true,
-      success: function(data) {
-        console.log("FCC", data);
-        isFccOnline(data); // This is the callback
-      }
-    });
-  };
-
-  getNameAndLogo(); // starts here.
-  getFccFollowers();
-
-  function areUsersOnline(data) {
-    const streamers = data.follows;
-    let renderFollowers = [],
-      logo, link, name, followers, views, isOnline;
-
-    // Render corresponding data for each follower of fcc.
-    streamers.forEach(function(streamer) {
-      logo = streamer.channel.logo || 'https://www.appointbetterboards.co.nz/Custom/Appoint/img/avatar-large.png';
-      link = streamer.channel.url;
-      name = streamer.channel.display_name;
-      followers = streamer.channel.followers;
-      views = streamer.channel.views;
-      console.log(streamer.channel);
-
-      // bootstrap rows are being constructed.
-      renderFollowers.push(
-        '<div class="row center-block">' +
-          '<div class="link-effect">' +
-            '<div id="logo" class="col-xs-3 col-sm-2">' +
-              '<a href="' + link + '" target="_blank">' +
-                '<img src="' + logo + '">' +
-              '</a>' +
-            '</div>' +
-          '</div>' +
-        '<div id="title" class="col-xs-6 col-sm-8 center-block">' + name + '</div>' +
-          '<div id="status" class="col-xs-3 col-sm-1">' +
-            '<div id="' + name + '" ></div>' +
+  const StreamerInformation = (function() {
+    const CLIENT_ID = 'sh9x4gbft40ht9pg5mk0p6sfvn1h3a';
+    // TODO: Each of these methods will take an object of a user's information & return
+    // jquery string to be rendered in the DOM.
+    function _renderNonExistantStreamer(streamer) {}
+    function _renderOfflineStreamer(streamer) {
+      console.log("offline renderer is firing off")
+      let streamerHTML =
+      '<div class="row center-block">' +
+        '<div class="link-effect">' +
+          '<div id="logo" class="col-xs-3 col-sm-2">' +
+            '<a href="' + streamer.link + '" target="_blank">' +
+              '<img src="' + streamer.logo + '">' +
+            '</a>' +
           '</div>' +
         '</div>' +
-        '</div>'
-      );
-    });
-    // then bootstrap rows are being rendered here.
-    $('#fcc-followers-dropoff').html(renderFollowers.join(""));
+      '<div id="stream-body" class="col-xs-6 col-sm-8 center-block">' +
+         '<h3>' + streamer.display_name + '</h3>' +
+      '</div>' +
+        '<div id="status" class="col-xs-3 col-sm-1">' +
+          '<div id="' + streamer.name + '"><i class="fa fa-bolt fa-3x" aria-hidden="true"></i></div>' +
+        '</div>' +
+      '</div>' +
+      '</div>';
+      return $(streamer.div).html(streamerHTML);
+    }
+    function _renderOnlineStreamer(streamer) {}
 
+    /* STEP THREE: Since user is offline, we gather information to populate row */
+    function getOfflineStreamerInformation(callbackData) {
+      let url = "https://api.twitch.tv/kraken/users/" + callbackData.name;
+      let streamerObj = {};
 
-    streamers.forEach(function(streamer, index) {
-      let url = 'https://api.twitch.tv/kraken/streams/' + streamers[index].channel.name;
+      $.ajax({
+        type: "GET",
+        url: url,
+        headers: {
+          'Client-ID': CLIENT_ID
+        },
+        success: function(data) {
+          data.link = callbackData.url;
+          data.div = callbackData.div;
+          console.log("offline", data);
+          return _renderOfflineStreamer(data);
+        }
+
+      });
+    }
+    // STEP TWO: Check if streamer is currently online.
+    function isStreamerOnline(callback) {
+      let url = 'https://api.twitch.tv/kraken/streams/' + callback.name;
       $.ajax({
         type: 'GET',
         url: url,
-        headers: { 'Client-ID': CLIENT_ID },
+        headers: {
+          'Client-ID': CLIENT_ID
+        },
         success: function(data) {
-          name = streamer.channel.display_name;
-          if (data.stream === null) { // if offline
-            $('#' + name).html('<i class="fa fa-bolt fa-3x" aria-hidden="true"></i>');
-          } else { // if onfine
-            $('#' + name).html('<i class="fa fa-globe fa-2x" aria-hidden="true"></i>');
+          console.log("STREAMER STATUS", data);
+          if (data.status == null) {
+            getOfflineStreamerInformation(callback);
+          } else {
+            console.log("YOU NEED TO DO SOMETHING FOR ONLINE USER HERE!");
           }
+        },
+        error: function (failure) {
+          console.log("STREAM STATUS FAILURE", failure);
         }
       });
-    })
+    }
 
-  }
-  // Get freeCodeCamp's followers
-  function getFccFollowers() {
-    var url = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels";
-    $.ajax({
-      type: 'GET',
-      url: url,
-      headers: {
-        'Client-ID': CLIENT_ID
-      },
-      async: true,
-      success: function(data) {
-        areUsersOnline(data); // <--- Here's the callback.
-      }
-    });
-  }
+    function retrieveStreamerFollowers() {
+      
+    }
+    // STEP ONE: Check if the specified streamer exists.
+    function validateStreamer(streamerName, targetDiv) {
+      let url = 'https://api.twitch.tv/kraken/channels/' + streamerName;
+      $.ajax({
+        type: 'GET',
+        url: url,
+        headers: {
+          'Client-ID': CLIENT_ID
+        },
+        success: function(data) {
+          console.log("IT WORKED", data);
+          data.div = targetDiv;
+          isStreamerOnline(data);
+        },
+        error: function (failure) {
+          let error = JSON.parse(failure.responseText);
+          error.div = targetDiv;
+          error.name = streamerName;
+          console.log("ERROR", error);
+        }
+      });
+    }
+
+    return {
+      validateStreamer: validateStreamer
+    };
+  })();
+
+  StreamerInformation.validateStreamer(["freecodecamp"], "#freeCodeCamp");
+
 
 });
